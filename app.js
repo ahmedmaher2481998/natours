62bd75539d0146cd3171acaa414482e3969d5524
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-07-26 12:31:11 +0200

    mounting the routers

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const fs = require('fs');
 const morgan = require('morgan');
 
 //middleware
-const toursEnpPoint = '/api/1/tours';
+const toursEndPoint = '/api/1/tours';
+const usersEndPoint = '/api/1/users';
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+//third party middleware
 app.use(morgan('dev'));
 app.use(express.json());
+
+//our own middleware
 app.use((req, res, next) => {
   console.log('Hello from your own middleWare..');
   next();
 });
+
 // route handlers
 const getAllTours = (req, res) => {
   res.status(200).json({
@@ -21,6 +27,7 @@ const getAllTours = (req, res) => {
     data: { tours },
   });
 };
+
 const GetTourById = (req, res) => {
   const id = +req.params.id;
   const requestedTour = tours.find((el) => el.id === id);
@@ -35,6 +42,7 @@ const GetTourById = (req, res) => {
     data: requestedTour,
   });
 };
+
 const createTour = (req, res) => {
   const newTour = {
     id: tours[tours.length - 1].id + 1,
@@ -49,6 +57,7 @@ const createTour = (req, res) => {
     });
   });
 };
+
 const UpdateTour = (req, res) => {
   //check if tour exists
   if (req.params.id * 1 > tours.length - 1) {
@@ -60,9 +69,10 @@ const UpdateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     body: req.body,
-    data: '<Updated Tour here ...',
+    data: '<Updated Tour here ...>',
   });
 };
+
 const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length - 1) {
     res.status(404).json({
@@ -75,21 +85,59 @@ const deleteTour = (req, res) => {
     data: null,
   });
 };
-// //get all tours
-// app.get(`${toursEnpPoint}`, getAllTours);
-// //get tour by id
-// app.get(`${toursEnpPoint}/:id`, GetTourById);
-// //create new tour
-// app.post(`${toursEnpPoint}`, createTour);
-// //patch tour
-// app.patch(`${toursEnpPoint}/:id`, UpdateTour);
-// // delete tour
-// app.delete(`${toursEnpPoint}/:id`, deleteTour);
-//routes
-app.route(toursEnpPoint).get(getAllTours).post(createTour);
-app.route(`${toursEnpPoint}/:id`).get(GetTourById).patch(UpdateTour).delete(deleteTour);
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Failed',
+    message: 'This is route is not implemented yet ....',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Failed',
+    message: 'This is route is not implemented yet ....',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Failed',
+    message: 'This is route is not implemented yet ....',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Failed',
+    message: 'This is route is not implemented yet ....',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Failed',
+    message: 'This is route is not implemented yet ....',
+  });
+};
+//mounting the routers
+const toursRouter = express.Router();
+const usersRouter = express.Router();
+
+//Tours Routes
+toursRouter.route('/').get(getAllTours).post(createTour);
+toursRouter.route(`/:id`).get(GetTourById).patch(UpdateTour).delete(deleteTour);
+
+//users Routes
+usersRouter.route('/').get(getAllUsers).post(createUser);
+usersRouter.route(`/:id`).get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use(toursEndPoint, toursRouter);
+app.use(usersEndPoint, usersRouter);
 
 //star the server
+
 app.listen(port, () => {
-  console.log('App running on port' + port);
+  console.clear();
+  console.log('\x1b[35m', `App running on port ${port}...`);
 });
