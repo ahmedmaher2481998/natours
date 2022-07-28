70be94200e6e37c8dfc0a89c66a0744e82647b91
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-07-28 17:40:58 +0200

    added mongoose

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //star the server
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+mongoose.connect();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('\x1b[35m', `App running on port ${port}...`);
