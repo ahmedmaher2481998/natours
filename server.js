619fb0301d16a1e8d141d29b3e45b805d651e298
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-07-28 19:00:55 +0200

    connecting to atlas using  mongoose

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-mongoose.connect();
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD).replace(
+  '<USER>',
+  process.env.DB_USER
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  //eslint-disable-next-line
   console.log('\x1b[35m', `App running on port ${port}...`);
 });
